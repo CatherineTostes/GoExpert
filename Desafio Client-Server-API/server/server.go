4d89827471ccd42b25a2a5625eb0edf42532bd45
Catherine Tostes <catherinetostes@gmail.com>
2023-11-13 21:59:30 -0300

    Add files via upload

---
+++ Desafio Client-Server-API/server/server.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type (
+	Cotacao struct {
+		Usdbrl Usdbrl `json:"USDBRL"`
+	}
+
+	Usdbrl struct {
+		Code       string `json:"code"`
+		Codein     string `json:"codein"`
+		Name       string `json:"name"`
+		High       string `json:"high"`
+		Low        string `json:"low"`
+		VarBid     string `json:"varBid"`
+		PctChange  string `json:"pctChange"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"ask"`
+		Timestamp  string `json:"timestamp"`
+		CreateDate string `json:"create_date"`
+	}
+)
+
+func main() {
+	http.HandleFunc("/cotacao", BuscaCotacaoHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func BuscaCotacaoHandler(w http.ResponseWriter, r *http.Request) {
+	cotacao, err := BuscaCotacao()
+	if err != nil {
+		return
+	}
+	usdbrl := convertToEntity(cotacao)
+	err = insertDatabase(usdbrl)
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(usdbrl.Bid)
+}
+
+func BuscaCotacao() (*Cotacao, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+	resp, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var cotacao Cotacao
+	err = json.Unmarshal(body, &cotacao)
+	select {
+	case <-time.After(200 * time.Millisecond):
+		log.Println("Tempo excedido na requisição")
+	case <-ctx.Done():
+		log.Println("Processado com sucesso")
+	}
+	return &cotacao, nil
+}
+
+func insertDatabase(usdbrl *Usdbrl) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open("db.sqlite"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	db.AutoMigrate(&Usdbrl{})
+	db.Create(usdbrl)
+	select {
+	case <-time.After(10 * time.Millisecond):
+		log.Println("Tempo excedido inserção dos dados no banco")
+	case <-ctx.Done():
+		log.Println("Processado com sucesso")
+	}
+	return nil
+}
+
+func convertToEntity(cotacao *Cotacao) *Usdbrl {
+	return &Usdbrl{
+		Code:       cotacao.Usdbrl.Code,
+		Codein:     cotacao.Usdbrl.Codein,
+		Name:       cotacao.Usdbrl.Name,
+		High:       cotacao.Usdbrl.High,
+		Low:        cotacao.Usdbrl.Low,
+		VarBid:     cotacao.Usdbrl.VarBid,
+		PctChange:  cotacao.Usdbrl.PctChange,
+		Bid:        cotacao.Usdbrl.Bid,
+		Ask:        cotacao.Usdbrl.Ask,
+		Timestamp:  cotacao.Usdbrl.Timestamp,
+		CreateDate: cotacao.Usdbrl.CreateDate,
+	}
+}
