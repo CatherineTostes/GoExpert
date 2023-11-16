bf3fb9ed685a0847cd5ee1657fabd3a43e841282
Catherine Tostes <catherinetostes@gmail.com>
2023-11-16 14:51:03 -0300

    Add files via upload

--- Desafio Client-Server-API/client/client.go
+++ Desafio Client-Server-API/client/client.go
@@ -12,30 +12,32 @@ import (
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
 	defer cancel()
-	req, err := http.NewRequestWithContext(context.TODO(), "GET", "http://localhost:8080/cotacao", nil)
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	arq, err := os.Create("cotacao.txt")
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	defer arq.Close()
 	_, err = arq.Write([]byte("Dólar: " + string(body)))
-	select {
-	case <-ctx.Done():
-		log.Println("Processado com sucesso")
-	case <-time.After(300 * time.Millisecond):
-		log.Println("Tempo excedido na requisição 1")
+	if err != nil {
+		log.Println(err)
+		panic(err)
 	}
 
 }
